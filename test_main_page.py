b0a75c285ef1ebd89e9ed4fe6150c9e73c9c94ce
0leShera <ole_shera@mail.ru>
2021-11-14 14:07:07 +0300

    add Pages folder

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/"
 
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+   browser.get(link)
+   go_to_login_page(browser)
