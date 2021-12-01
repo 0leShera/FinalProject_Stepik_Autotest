33c4a8d014cc0e2f2b8963c90bca0de1230f7378
Olesya <89737539+0leShera@users.noreply.github.com>
2021-12-01 23:11:50 +0300

    Add comment for should_be_authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -50,7 +50,7 @@ class BasePage():
 
     def should_be_authorized_user(self):
         assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
-                                                                 " probably unauthorised user"
+                                                                 " probably unauthorised user. - Please restart the test."
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
